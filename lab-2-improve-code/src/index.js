7b9ae4f0a52848893dce4ae345a831dd3ac45db9
Alfie Binnie <dcostam7@gmail.com>
2019-11-07 11:59:44 +0000

    One more nStr fix

--- lab-2-improve-code/src/index.js
+++ lab-2-improve-code/src/index.js
@@ -6,7 +6,7 @@ module.exports = function greeting(i = 'my friend') {
 
 out = 'Hello, ';
 
-  nStr = '';
+  let nStr = '';
   for (let j in i ) { let nStr = nStr + i[j] + ((parseInt(j)+2==i.length)?', and ': ((parseInt(j)+1==i.length)?'':', ')); }
 
     if (nStr.toUpperCase() === nStr) return "HELLO " + nStr +  '!'
